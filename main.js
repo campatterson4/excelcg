04c338db710160addb48b436adfa109ee1d63d89
Camryn <camrynpatterson@yahoo.com>
2023-01-03 15:26:11 -0600

    restructuring and resetting files

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 ///////////NAVBAR
 const header = document.querySelector('header');
 
+window.addEventListener('scroll', fixedNav);
+
 function fixedNav() {
     header.classList.toggle('scrolled', window.scrollY > 0);
 }
-
-window.addEventListener('scroll', fixedNav);
-
